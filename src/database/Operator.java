b367c284f97528dbbe48c37781d784e47a1371d0
Xinzhe Yang <xinzhe.y@gmail.com>
2018-09-16 22:23:51 -0400

    init abstract class

---
+++ src/database/Operator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package database;
+
+/**
+ * @author xinzheyang
+ *
+ */
+public abstract class Operator {
+	/**
+	 * @return
+	 */
+	public abstract Tuple getNextTuple();
+	/**
+	 * 
+	 */
+	public abstract void reset();
+}
---
+++ src/database/Tuple.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package database;
+
+/**
+ * @author xinzheyang
+ *
+ */
+public class Tuple {
+	
+}
