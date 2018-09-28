fb848abc490647263df7c521e5f2bdb20694f6ed
Xinqi Lyu <xinqi@dhcp-hol-4101.eduroam.cornell.edu>
2018-09-28 15:32:36 -0400

    wrote some scan

--- src/operator/ScanOperator.java
+++ src/operator/ScanOperator.java
@@ -3,8 +3,13 @@
  */
 package operator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
-
+import database.DBCatalog;
 import database.Tuple;
 
 /**
@@ -13,11 +18,12 @@ import database.Tuple;
  */
 public class ScanOperator extends Operator {
 	
+	private String tb;
 	/* Upon initialization, opens up a file scan on the appropriate data file
 	 * data file.
 	 */
-	public ScanOperator() {
-		
+	public ScanOperator(String tableName) {
+		tb = tableName;
 	}
 
 	/* Reads the next line from the file that stores the base table and 
@@ -26,7 +32,23 @@ public class ScanOperator extends Operator {
 	@Override
 	public Tuple getNextTuple() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		try {
+			BufferedReader f = new BufferedReader(new FileReader(this.tb));
+//			String[] cols = DBCatalog.getTableColumns(this.tb);
+//			String values = f.readLine();
+			String values;
+			if((values=f.readLine()) != null) {
+				return new Tuple(values,this.tb);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("fail to read table file");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/* (non-Javadoc)
