a5f9d63871f0d546e04c9bb87ed25b7541a9ddb3
Sitian Chen <sitianchen@dhcp-rhodes-2086.redrover.cornell.edu>
2018-10-15 11:20:57 -0400

    writer

--- src/database/TupleWriter.java
+++ src/database/TupleWriter.java
@@ -5,6 +5,7 @@ package database;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -17,19 +18,28 @@ public class TupleWriter {
 	private Tuple tuple;
 	private FileChannel fc;
 	private ByteBuffer buffer;
+	private FileOutputStream fout;
 	private static final int PAGE_SIZE = 4096;
 	
 	public TupleWriter(String fileout) {
 		try {
-			FileOutputStream fout = new FileOutputStream(fileout);
+			fout = new FileOutputStream(fileout);
 			fc = fout.getChannel();
 			buffer = ByteBuffer.allocate(PAGE_SIZE);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}	
+		
+	}
+	
+	public void close() {
+		try {
+			fout.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		
 	}
 	
 	/** Gets the instance's tuple
@@ -50,7 +60,15 @@ public class TupleWriter {
 	 * @param fileout: the file path to be written out to
 	 */
 	public void write() {
-		
+		for(int val : tuple.getColValues()) {
+			buffer.putInt(val);
+		}
+		try {
+			fc.write(buffer);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
