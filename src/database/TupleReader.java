8826f3fa3a63db7ea2b14c133497b841ad779e45
Xinqi Lyu <xinqi@dhcp-hol-4035.eduroam.cornell.edu>
2018-10-24 09:31:05 -0400

    add comments for BNLJoin

--- src/database/TupleReader.java
+++ src/database/TupleReader.java
@@ -3,7 +3,6 @@
  */
 package database;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
--- src/physicaloperator/BNLJoinOperator.java
+++ src/physicaloperator/BNLJoinOperator.java
@@ -11,7 +11,8 @@ import visitor.EvaluateExpVisitor;
 import net.sf.jsqlparser.expression.Expression;
 
 /**
- * The operator gets two child operators and join their tuples together
+ * The operator gets two child operators and uses Block-Nested Loop Join to
+ * join their tuples together
  * @author xinqi
  *
  */
@@ -20,25 +21,14 @@ public class BNLJoinOperator extends JoinOperator {
 	private int buffer_size; //	the number of “pages” to devote to each block of the outer relation
 	private Tuple rightTuple;
 	private int index;
+	
 	/**
-	 * Initializes the Join operator with condition given
 	 * @param left the left operator to be joined
 	 * @param right the right operator to be joined
 	 * @param condition the Expression condition that filters the tuples
+	 * @param size the size of the buffer in pages
 	 */
 	public BNLJoinOperator(Operator left, Operator right, Expression condition, int size) {
-//		leftChild = left;
-//		rightChild = right;
-//		
-//		columnIndexMap = leftChild.getColumnIndexMap(); //same col index map as child operator
-//		int leftSize = leftChild.getColumnIndexMap().size();
-//		HashMap<String,Integer> rightMap = rightChild.getColumnIndexMap();
-//		for(String s: rightMap.keySet()) {
-//			columnIndexMap.put(s, rightMap.get(s)+leftSize);
-//		}
-//		joinCondition = condition;
-//		leftTuple = leftChild.getNextTuple();
-//		visitor = new EvaluateExpVisitor();
 		super(left,right,condition);
 		buffer_size = size;
 		buffer = new ArrayList<>();
@@ -47,7 +37,7 @@ public class BNLJoinOperator extends JoinOperator {
 	}
 	
 	/**
-	 * Initializes the Join operator without condition given
+	 * Initializes the BNLJoin operator without condition given
 	 * @param left the left operator to be joined
 	 * @param right the right operator to be joined
 	 */
@@ -55,6 +45,10 @@ public class BNLJoinOperator extends JoinOperator {
 		this(left, right, null, size);
 	}
 	
+	/**
+	 * Clear the previous elements in the buffer and fill it with new tuples from the 
+	 * outer relation
+	 */
 	public void updateBuffer() {
 		buffer.clear();
 		int numTuples = 4096 * buffer_size / (4 * leftChild.getColumnIndexMap().size());
@@ -71,27 +65,6 @@ public class BNLJoinOperator extends JoinOperator {
 	 */
 	@Override
 	public Tuple getNextTuple() {
-//		while(leftTuple != null) {
-//			Tuple rightTuple = rightChild.getNextTuple();
-//			while (rightTuple != null) {
-//				//combine the two tuples and check if they pass the result;
-//				Tuple combinedTuple = leftTuple.merge(rightTuple);
-//				if(joinCondition == null) return combinedTuple;
-//				visitor.setCurrTuple(combinedTuple);
-//				visitor.setOperator(this);
-//				joinCondition.accept(visitor);
-//				boolean result = visitor.getReturnBoolValue();
-//				if(result) {
-//					return combinedTuple;
-//				}
-//				rightTuple = rightChild.getNextTuple();
-//			}
-//			leftTuple = leftChild.getNextTuple();
-//			rightChild.reset();
-//		}
-//		return null;
-		
-				
 		if(buffer.isEmpty()) updateBuffer();
 		//while there is another right tuple
 		while(!buffer.isEmpty()) {
