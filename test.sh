2b860e9900d778a0c4bd8bcdc83dee39f3e7e84b
Xinzhe Yang <xinzhe.y@gmail.com>
2018-10-25 20:07:38 -0400

    test bash script

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo start testing…...
+diff query1 ../expected/query1
+diff query2 ../expected/query2
+diff query3 ../expected/query3
+diff query4 ../expected/query4
+diff query5 ../expected/query5
+diff query6 ../expected/query6
+diff query7 ../expected/query7
+diff query8 ../expected/query8
+diff query9 ../expected/query9
+diff query10 ../expected/query10
+diff query11 ../expected/query11
+diff query12 ../expected/query12
+diff query13 ../expected/query13
+diff query14 ../expected/query14
+diff query15 ../expected/query15
