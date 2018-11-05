2f813de7cc482cd66b9d49e82fcd86671837344a
sitianchen <sc2294@cornell.edu>
2018-11-05 14:13:04 -0500

    clustered index

--- src/bplustree/BPlusTree.java
+++ src/bplustree/BPlusTree.java
@@ -1,6 +1,7 @@
 package bplustree;
 import database.DBCatalog;
 import database.TupleReader;
+import physicaloperator.ClusteredIndexSortOperator;
 import physicaloperator.InMemorySortOperator;
 import physicaloperator.ScanOperator;
 
@@ -23,65 +24,86 @@ public class BPlusTree {
 	private List<Node> leafNodes;
 	private int counter;
 	private TreeSerializer serializer;
-//	private boolean isClustered;
-	
+	private boolean isClustered;
+
 	public BPlusTree(String tableIn, String serializeLoc, boolean isClustered, int order) throws IOException {
 		fileName = DBCatalog.getTableLoc(tableIn);
 		serializer = new TreeSerializer(serializeLoc);
 		counter = 1; //counter initialized to 1
-//		this.isClustered = isClustered;
+		this.isClustered = isClustered;
 		//if clustered, sort and replace relation file, we use in memory sort for now.
 		if (isClustered) {
 			ScanOperator scan = new ScanOperator(tableIn, "");
-			InMemorySortOperator sort = new InMemorySortOperator(scan, new String[] {DBCatalog.getIndexKey(tableIn)});
+			ClusteredIndexSortOperator sort = new ClusteredIndexSortOperator(scan, DBCatalog.getIndexKey(tableIn));
 			sort.dump(fileName);
 		}
 	}
-	
-	public void scanAndConstructAll() throws IOException { //TODO: change to void method to scanAndConstruct?
-		HashMap<Integer, List<int []>> leafEntries = new HashMap<>();
-		TupleReader reader = new TupleReader(fileName);
-		int[] arr;
-		while ((arr=reader.getColNext(colIndex)) != null) {
-			int[] rid = {arr[1],arr[2]};
-			if(leafEntries.containsKey(arr[0])) {
-				leafEntries.get(arr[0]).add(new int[] {arr[1],arr[2]});
-			}
-			else {
-				List<int []> newList = new LinkedList<>();
-				newList.add(rid);
-				leafEntries.put(arr[0], newList);
+
+	public void scanAndConstructAll() throws IOException {
+		List<List<int []>> rids = new LinkedList<List<int []>>();
+		List<Integer> keyValues = new LinkedList<Integer>();
+
+		if (isClustered) { //clustered, tuples already sorted, no need to sort again
+			TupleReader reader = new TupleReader(fileName);
+			int[] arr;
+			while ((arr=reader.getColNext(colIndex)) != null) {
+				int[] rid = {arr[1],arr[2]};
+				if (keyValues.isEmpty() || keyValues.get(keyValues.size() - 1) != arr[0]) {
+					keyValues.add(arr[0]);
+					List<int []> newList = new LinkedList<>();
+					newList.add(rid);
+					rids.add(newList);
+				}
+				else {
+					rids.get(rids.size() - 1).add(rid);
+				}
 			}
 		}
-		
-		List<List<int []>> rids = new ArrayList<List<int []>>();
-		List<Integer> keyValues = new ArrayList<Integer>(leafEntries.keySet());
-		Collections.sort(keyValues);
-		for (Integer key : keyValues) {
-			rids.add(leafEntries.get(key)); //don't need to sort rids with this key because they're added in order of occurrence
+		else { //unclustered, need to sort data entries
+			HashMap<Integer, List<int []>> leafEntries = new HashMap<>();
+			TupleReader reader = new TupleReader(fileName);
+			int[] arr;
+			while ((arr=reader.getColNext(colIndex)) != null) {
+				int[] rid = {arr[1],arr[2]};
+				if(leafEntries.containsKey(arr[0])) {
+					leafEntries.get(arr[0]).add(new int[] {arr[1],arr[2]});
+				}
+				else {
+					List<int []> newList = new LinkedList<>();
+					newList.add(rid);
+					leafEntries.put(arr[0], newList);
+				}
+			}
+
+			rids = new ArrayList<List<int []>>();
+			keyValues = new ArrayList<Integer>(leafEntries.keySet());
+			Collections.sort(keyValues);
+			for (Integer key : keyValues) {
+				rids.add(leafEntries.get(key)); //don't need to sort rids with this key because they're added in order of occurrence
+			}
+			//		List<DataEntry> dataEntries = new ArrayList<>();
+			//		for (Map.Entry<Integer, List<int []>> entry : leafEntries.entrySet()) {
+			//		    int key = entry.getKey();
+			//		    List<int []> value = entry.getValue();
+			//		    DataEntry dataEntry = new DataEntry(key,value);
+			//		    dataEntries.add(dataEntry);
+			//		}
+			//		dataEntries.sort(new Comparator<DataEntry>() {
+			//			@Override
+			//			public int compare(DataEntry a, DataEntry b) {
+			//	        	if(a.getKey() < b.getKey()) return -1;
+			//	        	else if(a.getKey() > b.getKey()) return 1;
+			//		        return 0;
+			//			}
+			//		});
 		}
-//		List<DataEntry> dataEntries = new ArrayList<>();
-//		for (Map.Entry<Integer, List<int []>> entry : leafEntries.entrySet()) {
-//		    int key = entry.getKey();
-//		    List<int []> value = entry.getValue();
-//		    DataEntry dataEntry = new DataEntry(key,value);
-//		    dataEntries.add(dataEntry);
-//		}
-//		dataEntries.sort(new Comparator<DataEntry>() {
-//			@Override
-//			public int compare(DataEntry a, DataEntry b) {
-//	        	if(a.getKey() < b.getKey()) return -1;
-//	        	else if(a.getKey() > b.getKey()) return 1;
-//		        return 0;
-//			}
-//		});
-		
+
 		buildLeafNodes(keyValues, rids);
 		buildIndexNodes();
 		serializer.close();
-//		return rids;
+		//		return rids;
 	}
-	
+
 	/** Builds and serializes all leaf nodes of this BPlusTree.
 	 * @param entries
 	 * @return
@@ -92,7 +114,7 @@ public class BPlusTree {
 		int numOfEntries = allKeys.size();
 		int numOfNodes = (int) Math.ceil(numOfEntries/(2.0*order)); //number of remaining nodes
 		int curr=0;
-//		int leafTotalCount = numOfNodes; 
+		//		int leafTotalCount = numOfNodes; 
 		//address of node should be from 1 to leafTotalCount;
 		while(counter <= numOfNodes) {
 			int k = numOfEntries-curr;
@@ -100,40 +122,40 @@ public class BPlusTree {
 			if(numOfNodes == 2 && k>2*order && k<3*order) {
 				int n = (int) Math.ceil((double) k / 2);
 				leaf = new LeafNode(allKeys.subList(curr, curr+n), allRids.subList(curr, curr+n), counter);
-//				leaf = new LeafNode(entries.subList(curr, curr+n));
+				//				leaf = new LeafNode(entries.subList(curr, curr+n));
 				lst.add(leaf);
 				curr += n;
 			}
 			else {
 				int curEnd = Math.min(curr+2*order, numOfEntries);
 				leaf = new LeafNode(allKeys.subList(curr, curEnd), allRids.subList(curr, curEnd), counter);
-//				leaf = new LeafNode(entries.subList(curr, Math.min(curr+2*order, numOfEntries)));
+				//				leaf = new LeafNode(entries.subList(curr, Math.min(curr+2*order, numOfEntries)));
 				lst.add(leaf);
 				curr += 2*order;
-//				numOfEntries -= order;
+				//				numOfEntries -= order;
 			}
 			this.serializer.serialize(leaf);
-//			numOfNodes--;
+			//			numOfNodes--;
 			counter++;
 		}
 		//counter equals numOfNodes + 1 when exiting the while loop, ready for counting the index nodes. 
-//		counter = lst.size()+1; //the 
+		//		counter = lst.size()+1; //the 
 		leafNodes = lst;
-//		return lst;
+		//		return lst;
 	}
-	
+
 	/** Builds and serializes all index node of this tree.
 	 * @param leafNodes
 	 * @throws IOException
 	 */
 	private void buildIndexNodes() throws IOException {
 		//prev is null, construct the bottom layer: list of IndexNodes
-		
+
 		//prev not null 
-		
+
 		//BottomIndexNode
 		//OtherIndexNode
-//		counter++;
+		//		counter++;
 		List<Node> curr = new ArrayList<>();
 		List<Node> prev = leafNodes;
 
@@ -141,7 +163,7 @@ public class BPlusTree {
 			IndexNode node= new IndexNode(leafNodes, counter);
 			this.serializer.serialize(node);//serialize
 		}
-		
+
 		while(prev.size() != 1) {
 			int numOfChild = prev.size();//number of children from the previous lift
 			int numOfNodes = (int) Math.ceil(numOfChild/(2.0*order+1)); //number of index nodes in the level
@@ -157,13 +179,13 @@ public class BPlusTree {
 					//get the minimum value
 					int n = (int) Math.ceil((double) m / 2);
 					node= new IndexNode(prev.subList(indexCurr, indexCurr+n), counter);
-//					curr.add(node);
+					//					curr.add(node);
 					indexCurr += n;
-//					counter += n;
+					//					counter += n;
 				}
 				else {
 					node= new IndexNode(prev.subList(indexCurr, Math.min(indexCurr+2*order+1, numOfChild)), counter);
-//					curr.add(node);
+					//					curr.add(node);
 					indexCurr += 2*order+1;
 				}
 				curr.add(node);
@@ -176,24 +198,24 @@ public class BPlusTree {
 			prev=curr;
 			curr.clear();
 		}
-//		IndexNode d = new IndexNode();
-//		d.addPointer(leafNodes.get(0));
-//		for(int i=1;i<2*order+1;i++) {
-//			d.addKey(leafNodes.get(i).getMin());
-//			d.addPointer(leafNodes.get(i));
-//		}
-//		
-//		
+		//		IndexNode d = new IndexNode();
+		//		d.addPointer(leafNodes.get(0));
+		//		for(int i=1;i<2*order+1;i++) {
+		//			d.addKey(leafNodes.get(i).getMin());
+		//			d.addPointer(leafNodes.get(i));
+		//		}
+		//		
+		//		
 	}
-//<Node>children,address,keys,min
-//	public Node buildIndexNode(lst,start,end) {
-//		List<Integer> tmp = new ArrayList<>();
-//		address=counter;
-//		min=prev.get(0).get
-//		tmp.add(e)(leafNodes.get(0));
-//		for(int i=curr;i<n;i++) {
-//			
-//		}	
-//	}
+	//<Node>children,address,keys,min
+	//	public Node buildIndexNode(lst,start,end) {
+	//		List<Integer> tmp = new ArrayList<>();
+	//		address=counter;
+	//		min=prev.get(0).get
+	//		tmp.add(e)(leafNodes.get(0));
+	//		for(int i=curr;i<n;i++) {
+	//			
+	//		}	
+	//	}
 
 }
---
+++ src/physicaloperator/ClusteredIndexSortOperator.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package physicaloperator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import database.Tuple;
+
+/**
+ * @author sitianchen
+ *
+ */
+public class ClusteredIndexSortOperator extends Operator {
+//	private ScanOperator childOp;
+//	private String searchKey;
+	private LinkedList<Tuple> allTuples;
+	private int index;
+	
+	/** Uses unbounded state to get all tuples in the relation and sort them with the 
+	 * given search key value and their pageID, tupleID (which is just the order
+	 * of occurrence).  
+	 * @param childOp
+	 * @param searchKey
+	 */
+	public ClusteredIndexSortOperator(ScanOperator childOp, String searchKey) {
+		HashMap<Integer, List<Tuple>> keyMap = new HashMap<Integer, List<Tuple>>();
+		Tuple tuple;
+		columnIndexMap = childOp.getColumnIndexMap();
+		int keyIndex = columnIndexMap.get(searchKey);
+		while((tuple = childOp.getNextTuple()) != null) {
+			int colVal = tuple.getColumnValue(keyIndex);
+			if (keyMap.containsKey(colVal)) {
+				keyMap.get(colVal).add(tuple);
+			}
+			else {
+				List<Tuple> newLst = new LinkedList<Tuple>();
+				newLst.add(tuple);
+				keyMap.put(colVal, newLst);
+			}
+		}
+		List<Integer> keyValues = new ArrayList<Integer>(keyMap.keySet());
+		Collections.sort(keyValues);
+		for (Integer key : keyValues) {
+			allTuples.addAll(keyMap.get(key));
+		}
+		index = 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see physicaloperator.Operator#getNextTuple()
+	 */
+	@Override
+	public Tuple getNextTuple() {
+		if (index < allTuples.size()) {
+			Tuple cur = allTuples.get(index++);
+			return cur;
+		}
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see physicaloperator.Operator#reset()
+	 */
+	@Override
+	public void reset() {
+
+	}
+
+	/* (non-Javadoc)
+	 * @see physicaloperator.Operator#reset(int)
+	 */
+	@Override
+	public void reset(int index) {
+
+	}
+
+}
