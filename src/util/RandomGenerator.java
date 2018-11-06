ca28dda17d922c6de0b3f289f1b87018367d66dc
xinqilyu <xl358@cornell.edu>
2018-11-06 17:01:24 -0500

    wrote random generator

--- src/util/RandomGenerator.java
+++ src/util/RandomGenerator.java
@@ -4,6 +4,7 @@
 package util;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Random;
 
 import database.Tuple;
@@ -31,31 +32,63 @@ public class RandomGenerator {
 	 * @param lo the lower bound of random generator
 	 * @param hi the upper bound of random generator
 	 */
-	public void generate(String fname, int nRow, int nCol, int lo, int hi) {
-		TupleWriter tw = new TupleWriter(path + File.separator + fname, nCol);
-		tw.writeMetaData();
-		for (int i = 0; i < nRow; i++) {
-			StringBuilder sb = new StringBuilder();
-			for(int j=0; j< nCol; j++) {
-				Random rand = new Random(); 
+//	public void generate(String fname, int nRow, int nCol, int lo, int hi) {
+//		TupleWriter tw = new TupleWriter(path + File.separator + fname, nCol);
+//		tw.writeMetaData();
+//		for (int i = 0; i < nRow; i++) {
+//			StringBuilder sb = new StringBuilder();
+//			for(int j=0; j< nCol; j++) {
+//				Random rand = new Random(); 
+//				int randomNum = rand.nextInt((hi - lo) + 1) + lo;
+//				sb.append(randomNum);
+//				sb.append(",");
+//			}
+//			sb.deleteCharAt(sb.length()-1);
+//			Tuple tuple = new Tuple(sb.toString());
+//			tw.setTuple(tuple);
+//			tw.writeToBuffer();
+//		}
+//		tw.flushLastPage();
+//		tw.close();
+//	}
+	public void generate(String fname, int nRow, int nCol, int lo, int hi, int index) {
+	TupleWriter tw = new TupleWriter(path + File.separator + fname, nCol);
+	tw.writeMetaData();
+	HashMap<Integer, Integer> map = new HashMap<>();
+	Random rand = new Random(); 
+	for (int i = 0; i < nRow; i++) {
+		StringBuilder sb = new StringBuilder();
+		for(int j=0; j< nCol; j++) {
+			if(j==index) {
+				int randomNum = rand.nextInt((hi - lo) + 1) + lo;
+				while(map.containsKey(randomNum) && map.get(randomNum)>=4) {
+					randomNum=rand.nextInt();
+				};
+				map.put(randomNum, map.getOrDefault(randomNum, 0)+1);
+				sb.append(randomNum);
+				sb.append(",");
+			}
+			else {
 				int randomNum = rand.nextInt((hi - lo) + 1) + lo;
 				sb.append(randomNum);
 				sb.append(",");
 			}
-			sb.deleteCharAt(sb.length()-1);
-			Tuple tuple = new Tuple(sb.toString());
-			tw.setTuple(tuple);
-			tw.writeToBuffer();
+//			Random rand = new Random(); 
 		}
-		tw.flushLastPage();
-		tw.close();
+		sb.deleteCharAt(sb.length()-1);
+		Tuple tuple = new Tuple(sb.toString());
+		tw.setTuple(tuple);
+		tw.writeToBuffer();
 	}
+	tw.flushLastPage();
+	tw.close();
+}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		RandomGenerator randomGenerator = new RandomGenerator("random");
-		randomGenerator.generate("Sailors", 1000, 3, 0, 1500);
+		randomGenerator.generate("Boats", 1000, 3, 0, 800, 0);
 
 	}
 
