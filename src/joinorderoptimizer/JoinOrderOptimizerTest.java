d473e615b80bf56bd1138ab9e0f14ae641ada750
sitianchen <sc2294@cornell.edu>
2018-11-26 21:11:34 -0500

    basic testing

--- src/joinorderoptimizer/JoinOrderOptimizerTest.java
+++ src/joinorderoptimizer/JoinOrderOptimizerTest.java
@@ -5,8 +5,12 @@ package joinorderoptimizer;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
+import logicaloperator.LogicalJoinOperator;
 import logicaloperator.LogicalOperator;
 import logicaloperator.LogicalScanOperator;
 
@@ -23,7 +27,40 @@ public class JoinOrderOptimizerTest {
 		LogicalScanOperator scanC = new LogicalScanOperator("Boats", "B");
 		scanA.setRelationSize(1000);
 		scanB.setRelationSize(5000);
-		fail("Not yet implemented");
+		scanC.setRelationSize(7000);
+		List<LogicalOperator> listAB = new LinkedList<LogicalOperator>();
+		listAB.add(scanA);
+		listAB.add(scanB);
+		LogicalJoinOperator joinAB = new LogicalJoinOperator(listAB, null);
+		JoinOrderOptimizer optAB = new JoinOrderOptimizer(joinAB, null);
+		optAB.dpChooseBestPlan();
+		List<LogicalOperator> bestAB = optAB.getBestOrder();
+		assert bestAB.get(0).equals(scanA);
+		assert bestAB.get(1).equals(scanB);
+		
+		List<LogicalOperator> listCB = new LinkedList<LogicalOperator>();
+		listCB.add(scanC);
+		listCB.add(scanB);
+		
+		LogicalJoinOperator joinCB = new LogicalJoinOperator(listCB, null);
+		JoinOrderOptimizer optCB = new JoinOrderOptimizer(joinCB, null);
+		optCB.dpChooseBestPlan();
+		List<LogicalOperator> bestCB = optCB.getBestOrder();
+		assert bestCB.get(0).equals(scanB);
+		assert bestCB.get(1).equals(scanC);
+		
+		List<LogicalOperator> listABC = new LinkedList<LogicalOperator>();
+		listABC.add(scanA);
+		listABC.add(scanB);
+		listABC.add(scanC);
+		
+		LogicalJoinOperator joinABC = new LogicalJoinOperator(listABC, null);
+		JoinOrderOptimizer optABC = new JoinOrderOptimizer(joinABC, null);
+		optABC.dpChooseBestPlan();
+		List<LogicalOperator> bestABC = optABC.getBestOrder();
+		assert bestABC.get(0).equals(scanA);
+		assert bestABC.get(1).equals(scanB);
+		assert bestABC.get(2).equals(scanC);
 	}
 
 }
